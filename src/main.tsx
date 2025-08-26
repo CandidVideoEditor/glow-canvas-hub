63aad0dea2da48cc319848bcf34a45c8bb0c8415
CandidVideoEditor <candidvideoeditor@gmail.com>
2025-08-26 17:17:54 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MusicPlayer from './MusicPlayer';
-import './index.css'; // Assuming you're using Tailwind CSS
+import MusicPlayer from "./MusicPlayer";
 
 function App() {
   return (
     <div>
-      <h1>Welcome to My Website</h1>
-      {/* Other content */}
+      <h1>Welcome to Glow Canvas Hub</h1>
       <MusicPlayer />
     </div>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
